1778a04d453a7748d9d3bfa7c186c7ea3838841a
Daniel Edgar <aknot242@gmail.com>
2023-12-05 14:39:26 -0700

    fix

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var express = require('express')
 var app = express()
 
 function if_header_defined_return(request, headerName) {
-    return request.header('X-Forwarded-For') != undefined ? `${headerName}: ${request.header(headerName)}` : ''
+    return request.header(headerName) != undefined ? `${headerName}: ${request.header(headerName)}` : ''
 }
 
 //Define request response in root URL (/) and response with text Hello World!
 app.get('/', function (req, res) {
     res.send(`Hello, ${process.env.NAME}!<br>&nbsp;<br>&nbsp;<br>\
     <h3>Defined Proxy Headers</h3>\
+    ${if_header_defined_return(req, 'Host')}<br>\
     ${if_header_defined_return(req, 'X-Forwarded-For')}<br>\
     ${if_header_defined_return(req, 'X-Forwarded-Port')}<br>\
     ${if_header_defined_return(req, 'X-Forwarded-Proto')}<br>\
